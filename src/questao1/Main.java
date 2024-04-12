34cf8f9ea805e541771a0f7aebff3fb0ce48b12a
Therlyson Ryan <therlysonsousa@gmail.com>
2024-04-12 19:33:30 -0300

    Adicionando o generics ao algoritmo mergeInsert

--- src/questao1/Main.java
+++ src/questao1/Main.java
@@ -1,30 +1,39 @@
 package questao1;
 
-import java.util.Arrays;
+import classesGerais.CriaVetor;
+import classesGerais.Generic;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int[] array = new int[100];
-        for(int i =0; i<array.length; i++){
-            array[i] = (int) (Math.random() * array.length);
+        System.out.print("Qual o tamanho do vetor que você deseja ordenar: ");
+        int tamanho = scanner.nextInt();
+        System.out.print("\n1 - inteiro\n2 - String\n3 - double\n" +
+                "Digite a opção do tipo de vetor que deseja ordenar: ");
+        int opcao = scanner.nextInt();
+
+        CriaVetor vetor = new CriaVetor();
+        List<Generic> vetorCriado = vetor.criar(tamanho, opcao);
+
+        System.out.println("\nVetor não ordenado: ");
+        for(Generic v: vetorCriado){
+            System.out.print(v.getValue() + " ");
         }
 
-
-
-        System.out.println("desordenado: ");
-        for(int i =0; i<array.length; i++){
-            System.out.print(array[i] + " ");
-        }
-
-        int[] aux = new int[array.length];
+        List<Generic> auxiliar = new ArrayList<>(Collections.nCopies(vetorCriado.size(), null));
 
         MergeInsert mergeInsert = new MergeInsert();
-        mergeInsert.mergeSort(array, aux, 0, array.length-1);
-        System.out.println("\nOrdenado: ");
-        for(int i =0; i<array.length; i++){
-            System.out.print(array[i] + " ");
+        mergeInsert.mergeSort(vetorCriado, auxiliar, 0, vetorCriado.size()-1);
+
+        System.out.println("\nVetor ordenado: ");
+        for(Generic v: vetorCriado){
+            System.out.print(v.getValue() + " ");
         }
     }
 }
--- src/questao1/MergeInsert.java
+++ src/questao1/MergeInsert.java
@@ -1,10 +1,15 @@
 package questao1;
 
-public class MergeInsert {
+import classesGerais.Generic;
 
-    public void mergeSort(int[] vetor, int[] auxiliar, int inicio, int fim){
+import java.util.List;
+
+public class MergeInsert {
+    private final int MOMENTO_INSERT = 20;
+
+    public void mergeSort(List<Generic> vetor, List<Generic> auxiliar, int inicio, int fim){
         if(inicio < fim){
-            if((fim-inicio) < 15){  //caso base o vetor tem 3 vetores
+            if((fim-inicio) < MOMENTO_INSERT){  //caso base o vetor tem 3 vetores
                 insertSort(vetor, inicio, fim);
                 return;
             }
@@ -12,7 +17,7 @@ public class MergeInsert {
             mergeSort(vetor, auxiliar, inicio, meio);
             mergeSort(vetor, auxiliar, meio+1, fim);
 
-            if(vetor[meio] <= vetor[meio+1]){
+            if(vetor.get(meio).compareTo(vetor.get(meio+1)) <= 0){
                 return;
             }
 
@@ -20,41 +25,42 @@ public class MergeInsert {
         }
     }
 
-    private void intercalar(int[] vetor, int[] auxiliar, int inicio, int meio, int fim) {
+    private void intercalar(List<Generic> vetor, List<Generic> auxiliar, int inicio, int meio, int fim) {
         for(int i = inicio; i<=fim; i++){
-            auxiliar[i] = vetor[i];
+            auxiliar.set(i, vetor.get(i));
         }
         int esq = inicio;
         int dir = meio+1;
 
         for(int i = inicio; i<=fim; i++){
             if(esq>meio){
-                vetor[i] = auxiliar[dir++];
+                vetor.set(i, auxiliar.get(dir++));
             }
             else if(dir > fim){
-                vetor[i] = auxiliar[esq++];
+                vetor.set(i, auxiliar.get(esq++));
             }
-            else if(auxiliar[esq] <= auxiliar[dir]){
-                vetor[i] = auxiliar[esq++]; // vetor[0] = auxiliar[0]
+            else if(auxiliar.get(esq).compareTo(auxiliar.get(dir)) <= 0){
+                vetor.set(i, auxiliar.get(esq++));
             }
-            else if(auxiliar[dir] < auxiliar[esq]) {
-                vetor[i] = auxiliar[dir++];
+            else if(auxiliar.get(dir).compareTo(auxiliar.get(esq)) < 0) {
+                vetor.set(i, auxiliar.get(dir++));
             }
         }
     }
 
     // 1, 5, 14, 5, 7 ---> 1, 5, 7, 5, 7 ---> 1, 5, 7, 12, 7 ---> 1, 5, 7, 12, 14
 
-    private void insertSort(int[] array, int inicio, int fim){
-        int aux2, j;
+    private void insertSort(List<Generic> array, int inicio, int fim){
+        Generic aux2;
+        int j;
         for(int i=inicio+1; i<=fim; i++){
-            aux2 = array[i];
+            aux2 = array.get(i);
             j = i-1;
-            while(j>=inicio && array[j] > aux2){  //j >= inicio e não j>=0
-                array[j+1] = array[j];
+            while(j>=inicio && array.get(j).compareTo(aux2) > 0){  //j >= inicio e não j>=0
+                array.set(j+1, array.get(j));
                 j--;
             }
-            array[j+1] = aux2;
+            array.set(j+1, aux2);
         }
     }
 }
